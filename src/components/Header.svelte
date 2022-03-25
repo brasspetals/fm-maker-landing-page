adeed8e99722a3e3d7a0e6e90a8c3496c493d462
Anna <67719118+brasspetals@users.noreply.github.com>
2022-03-25 11:58:58 -0500

    Add base files and styling

---
+++ src/components/Header.svelte
@@ -0,0 +1,59 @@
+<script>
+
+</script>
+
+<header>
+  <div class="logo">
+    <a href="/"><img src="/images/logo.svg" alt="Maker - Home"></a>
+  </div>
+  <div class="hero">
+    <h1>Get paid for the work you <span>love</span> to do.</h1>
+    <p>The 9-5 grind is so last century. We believe in living life on your own terms. Whether you’re looking to escape the rat race or set up a side hustle, we’ve got you covered.</p>
+  </div>
+</header>
+
+<style>
+  header {
+    display: grid;
+    justify-items: center;
+    position: relative;
+    padding: 40px 0;
+  }
+
+  .hero {
+    background-image: url('/images/illustration-hero-mobile.png');
+    background-position-y: 60px;
+    background-position-x: center;
+    background-repeat: no-repeat;
+    background-size: 410px 233px;
+    padding: 342px 24px 65px;
+    text-align: center;
+    overflow: hidden;
+    display: grid;
+    justify-items: center;
+    gap: 24px;
+  }
+
+  h1 {
+    font-size: 32px;
+    line-height: 1.25;
+    padding: 0 38px;
+    max-width: 20ch;
+  }
+
+  span {
+    color: var(--color-cyan);
+  }
+
+  p {
+    max-width: 42ch;
+  }
+
+  header::after {
+    content: url('/images/icon-scroll.svg');
+    position: absolute;
+    bottom: 26px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+</style>

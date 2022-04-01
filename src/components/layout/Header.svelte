801b2331d046a712dfdfff9b7be049c078ace845
Anna <67719118+brasspetals@users.noreply.github.com>
2022-04-01 16:24:49 -0500

    Update hero image responsiveness

--- src/components/layout/Header.svelte
+++ src/components/layout/Header.svelte
@@ -7,8 +7,14 @@
     <a href="/"><img src="/images/logo.svg" alt="Maker - Home"></a>
   </div>
   <div class="hero">
-    <h1>Get paid for the work you <span>love</span> to do.</h1>
-    <p>The 9-5 grind is so last century. We believe in living life on your own terms. Whether you’re looking to escape the rat race or set up a side hustle, we’ve got you covered.</p>
+    <div class="hero-img-1">
+      <img src="/images/illustration-hero-mobile.png" alt="">
+    </div>
+    <div class="hero-text">
+      <h1>Get paid for the work you <span>love</span> to do.</h1>
+      <p>The 9-5 grind is so last century. We believe in living life on your own terms. Whether you’re looking to escape the rat race or set up a side hustle, we’ve got you covered.</p>
+    </div>
+    <div class="hero-img-2"></div>
   </div>
 </header>
 
@@ -21,14 +27,22 @@
   }
 
   .hero {
-    background-image: url('/images/illustration-hero-mobile.png');
-    background-position-y: 60px;
-    background-position-x: center;
-    background-repeat: no-repeat;
-    background-size: 410px 233px;
-    padding: 342px 24px 0;
+    padding-top: 60px;
     text-align: center;
     overflow: hidden;
+    display: grid;
+    justify-items: center;
+    gap: 48px;
+  }
+
+  .hero img {
+    display: block;
+    width: 109%;
+    max-width: 650px;
+  }
+
+  .hero-text {
+    padding: 0 24px;
     display: grid;
     justify-items: center;
     gap: 24px;
@@ -47,6 +61,10 @@
 
   p {
     max-width: 42ch;
+  }
+
+  .hero-img-2 {
+    display: none;
   }
 
   header::after {

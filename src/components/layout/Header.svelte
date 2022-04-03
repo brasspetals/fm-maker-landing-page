cb280bfdba6d90c0f519624d38c25fdaadde0aab
Anna <67719118+brasspetals@users.noreply.github.com>
2022-04-02 20:06:04 -0500

    Add desktop header styles, overhaul hero images

--- src/components/layout/Header.svelte
+++ src/components/layout/Header.svelte
@@ -7,19 +7,11 @@
     <a href="/"><img src="/images/logo.svg" alt="Maker - Home"></a>
   </div>
   <div class="hero">
-    <div class="hero-img hero-img-left">
-      <picture>
-        <source media="(min-width: 700px)" srcset="/images/illustration-hero-left.svg">
-        <img src="/images/illustration-hero-mobile.png" alt="">
-      </picture>
-    </div>
+    <img class="hero-image-mobile" src="/images/illustration-hero-mobile.png" alt="">
     <div class="hero-text">
       <h1>Get paid for the work you <span>love</span> to do.</h1>
       <p>The 9-5 grind is so last century. We believe in living life on your own terms. Whether you’re looking to escape the rat race or set up a side hustle, we’ve got you covered.</p>
       <img class="scroll" src="/images/icon-scroll.svg" alt="">
-    </div>
-    <div class="hero-img hero-img-right">
-      <img src="/images/illustration-hero-right.svg" alt="">
     </div>
   </div>
 </header>
@@ -38,14 +30,14 @@
     text-align: center;
     display: grid;
     justify-items: center;
-    overflow-x: hidden;
     gap: 48px;
   }
 
-  .hero-img-left img {
+  .hero-image-mobile {
     display: block;
     width: 109%;
     max-width: 600px;
+    transform: translateX(17px);
   }
 
   .hero-text {
@@ -74,14 +66,9 @@
     margin-top: 8px;
   }
 
-  .hero-img-right {
-    display: none;
-  }
-
-
   @media screen and (min-width: 700px) {
     header {
-      padding: 48px 0;
+      padding: 48px 0 52px;
     }
 
     .logo {
@@ -91,19 +78,27 @@
 
     .hero {
       padding-top: 72px;
-      grid-template-columns: 1fr 475px 1fr;
-      gap: 0;
       align-items: start;
+      position: relative;
+      width: 100%;
     }
 
-    .hero-img img {
-      width: unset;
+    .hero-image-mobile {
+      display: none;
+    }
+
+    .hero-text::before {
+      content: '';
+      background-image: url('/images/illustration-hero-left.svg');
+      background-size: 314px auto;
+      background-repeat: no-repeat;
       height: 357px;
+      width: 314px;
+      position: absolute;
+      left: 50%;
+      transform: translateX(-176%);
+      top: 75px;
     }
-
-    .hero-img-right {
-      display: block;
-    } 
 
     .hero-text {
       padding: 52px 0;
@@ -112,6 +107,77 @@
       background-repeat: no-repeat;
       background-position-x: center;
       background-position-y: 33px;
+    }
+
+    .hero-text::after {
+      content: '';
+      background-image: url('/images/illustration-hero-right.svg');
+      background-size: 317px auto;
+      background-repeat: no-repeat;
+      height: 357px;
+      width: 317px;
+      position: absolute;
+      left: 50%;
+      transform: translateX(75%);
+      top: 73px;
+    }
+  }
+
+  @media screen and (min-width: 1200px) {
+    header {
+      padding: 81px 0;
+      margin: 0 auto;
+      max-width: 1554px;
+    }
+
+    .logo {
+      width: 100%;
+      max-width: 1110px;
+      margin: 0 auto;
+    }
+
+    .hero {
+      padding-top: 97px;
+      width: 100%;
+      overflow: hidden;
+    }
+
+    .hero-text::before {
+      background-size: 408px auto;
+      height: 459px;
+      width: 408px;
+      transform: translateX(-189.5%);
+      top: 108px;
+    }
+
+    .hero-text {
+      padding: 83px 0;
+      background-size: contain;
+      background-position-y: top;
+      max-width: 544px;
+    }
+
+    h1 {
+      font-size: 48px;
+      line-height: 1.1667;
+      padding: 0;
+      max-width: 18ch;
+    }
+
+    p {
+      max-width: 58ch;
+    }
+
+    .scroll {
+      margin-top: 41px;
+    }
+
+    .hero-text::after {
+      background-size: 396px auto;
+      height: 444px;
+      width: 396px;
+      transform: translateX(97%);
+      top: 117px;
     }
   }
 </style>
